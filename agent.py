d318e529f78a1d5cb405cf57aa48d12a3256dab2
German Paul <germanpaul@Germans-MacBook-Air.local>
2024-12-06 16:19:24 +0100

    add model and model loading

--- agent.py
+++ agent.py
@@ -14,10 +14,13 @@ class Agent:
 
     def __init__(self):
         self.n_games = 0
-        self.epsilon = 0 # randomness
+        self.epsilon = 0.0 # randomness
         self.gamma = 0.9 # discount rate
         self.memory = deque(maxlen=MAX_MEMORY) # popleft()
-        self.model = Linear_QNet(11, 256, 3)
+        #self.model = Linear_QNet(11, 256, 3)
+        self.model = Linear_QNet(11, 256, 3)  # Replace with your input, hidden, and output dimensions
+        self.model.load_state_dict(torch.load("./model/model.pth"))
+        self.model.eval()  # Set the model to evaluation mode
         self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
 
 
@@ -85,8 +88,9 @@ class Agent:
         self.trainer.train_step(state, action, reward, next_state, done)
 
     def get_action(self, state):
-        # random moves: tradeoff exploration / exploitation
-        self.epsilon = 80 - self.n_games
+        # random moves: tradeoff exploration / exploitation        
+        #self.epsilon = 80 - self.n_games # If new model then uncomment this line
+        self.epsilon = 0 - self.n_games # comment out if you train a new model
         final_move = [0,0,0]
         if random.randint(0, 200) < self.epsilon:
             move = random.randint(0, 2)
